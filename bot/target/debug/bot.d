2f4fd71d206f988fd3827400e6e61eddd456d34c
Limi <21beaps@gmail.com>
2022-08-05 13:39:27 +1200

    delete directory that will be git ignored

--- bot/target/debug/bot.d
+++
@@ -1,1 +0,0 @@
-C:\Users\21bea\Documents\Programming\pnw\rift\rift\rift\bot\target\debug\bot.exe: C:\Users\21bea\Documents\Programming\pnw\rift\rift\rift\bot\src\main.rs
--- bot/target/debug/deps/bot.d
+++
@@ -1,5 +0,0 @@
-C:\Users\21bea\Documents\Programming\pnw\rift\rift\rift\bot\target\debug\deps\bot.exe: src\main.rs
-
-C:\Users\21bea\Documents\Programming\pnw\rift\rift\rift\bot\target\debug\deps\bot.d: src\main.rs
-
-src\main.rs:
