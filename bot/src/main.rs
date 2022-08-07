142c643da397951ed817e4bf40538cbeaec61fc8
UnlimitedFrames <21beaps@gmail.com>
2022-08-07 19:29:47 +1200

    removed localisation

--- bot/src/main.rs
+++ bot/src/main.rs
@@ -116,26 +116,7 @@ impl EventHandler for Handler {
                                 .description_localized("de", "Die versendete Nachricht")
                                 .kind(CommandOptionType::String)
                                 .required(true)
-                                .add_string_choice_localized(
-                                    "Welcome to our cool server! Ask me if you need help",
-                                    "pizza",
-                                    [("de", "Willkommen auf unserem coolen Server! Frag mich, falls du Hilfe brauchst")]
-                                )
-                                .add_string_choice_localized(
-                                    "Hey, do you want a coffee?",
-                                    "coffee",
-                                    [("de", "Hey, willst du einen Kaffee?")],
-                                )
-                                .add_string_choice_localized(
-                                    "Welcome to the club, you're now a good person. Well, I hope.",
-                                    "club",
-                                    [("de", "Willkommen im Club, du bist jetzt ein guter Mensch. Naja, hoffentlich.")],
-                                )
-                                .add_string_choice_localized(
-                                    "I hope that you brought a controller to play together!",
-                                    "game",
-                                    [("de", "Ich hoffe du hast einen Controller zum Spielen mitgebracht!")],
-                                )
+                                
                         })
                 })
                 .create_application_command(|command| {
