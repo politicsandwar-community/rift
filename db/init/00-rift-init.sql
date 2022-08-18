c63af3b263d5bf399be985215aeda164ba49a123
UnlimitedFrames <21beaps@gmail.com>
2022-08-18 12:45:36 +1200

    removed notnulls

--- db/init/00-rift-init.sql
+++ db/init/00-rift-init.sql
@@ -1,14 +1,14 @@
 CREATE TYPE "resources" AS (
-  "money" numeric NOT NULL,
-  "coal" numeric NOT NULL,
-  "oil" numeric NOT NULL,
-  "uranium" numeric NOT NULL,
-  "iron" numeric NOT NULL,
-  "bauxite" numeric NOT NULL,
-  "lead" numeric NOT NULL,
-  "gasoline" numeric NOT NULL,
-  "munitions" numeric NOT NULL,
-  "steel" numeric NOT NULL,
-  "aluminum" numeric NOT NULL,
-  "food" numeric NOT NULL
+  "money" numeric,
+  "coal" numeric,
+  "oil" numeric,
+  "uranium" numeric,
+  "iron" numeric,
+  "bauxite" numeric,
+  "lead" numeric,
+  "gasoline" numeric,
+  "munitions" numeric,
+  "steel" numeric,
+  "aluminum" numeric,
+  "food" numeric
 );
