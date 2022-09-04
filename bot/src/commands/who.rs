71493cdf53d140bbd023975d6d603875eaecb648
Limi <limi@limi.info>
2022-09-04 18:16:26 +1200

    i cant remember lol

--- bot/src/commands/who.rs
+++ bot/src/commands/who.rs
@@ -10,7 +10,13 @@ async fn who(
 ) -> Result<(), Error> {
     let u = user.as_ref().unwrap_or_else(|| ctx.author());
     let gotten_user = ctx.data().cache.get_user(&i64::try_from(u.id.0).unwrap());
-    let response = format!("test {}",match gotten_user.unwrap().nation_id.unwrap_or() );
+    let response = format!(
+        "test {}",
+        gotten_user
+            .unwrap_or("No User Found")
+            .nation_id
+            .unwrap_or(0)
+    );
     ctx.say(response).await?;
     Ok(())
 }
