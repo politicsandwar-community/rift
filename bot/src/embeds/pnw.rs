7173f27f98e5dab63a514282b13a9bc45190cded
Limi <limi@limi.info>
2022-11-01 12:29:43 +1300

    yes ytes

--- bot/src/embeds/pnw.rs
+++ bot/src/embeds/pnw.rs
@@ -1,7 +1,7 @@
 use poise::serenity_prelude::CreateEmbed;
 use url::Url;
 
-use crate::{consts, structs::Nation, types::Context};
+use crate::{consts, strings::pnw, structs::Nation, types::Context};
 
 pub fn nation<'a>(
     ctx: &'a Context,
@@ -18,6 +18,10 @@ pub fn nation<'a>(
             },
         ))
         .colour(consts::embed::INFO_EMBED_COLOUR)
+        .description(pnw::link(
+            "Nation Page".to_string(),
+            format!("https://politicsandwar.com/nation/id={}", nation.id),
+        ))
         .fields([
             ("Nation ID", format!("{}", nation.id), true),
             ("Name", nation.name.to_string(), true),
@@ -28,14 +32,12 @@ pub fn nation<'a>(
             ("Color", nation.color.to_string(), true),
             (
                 "Alliance",
-                format!(
-                    "[{}]({})",
-                    nation.alliance_id,
-                    Url::parse(&format!(
+                pnw::link(
+                    nation.alliance_id.to_string(),
+                    format!(
                         "{}{}",
                         "https://politicsandwar.com/alliance/id=", nation.alliance_id
-                    ))
-                    .expect("somtinbroke")
+                    ),
                 ),
                 true,
             ),
@@ -44,7 +46,17 @@ pub fn nation<'a>(
                 nation.alliance_position.to_string(),
                 true,
             ),
-            ("Cities", format!("{}", nation.num_cities), true),
+            (
+                "Cities",
+                pnw::link(
+                    nation.num_cities.to_string(),
+                    format!(
+                        "{}{}",
+                        "https://politicsandwar.com/?id=62&l=", nation.leader
+                    ),
+                ),
+                true,
+            ),
             ("Score", format!("{}", nation.score), true),
             (
                 "Vacation Mode",
@@ -89,8 +101,78 @@ pub fn nation<'a>(
             ),
             ("Missiles", format!("{}", nation.missiles), true),
             ("Nukes", format!("{}", nation.nukes), true),
-            ("Average Infrastructure", "test".to_string(), true),
-            ("Average Land", "test".to_string(), true),
+            (
+                "Average Infrastructure",
+                "Not Implimented".to_string(),
+                true,
+            ),
+            ("Average Land", "Not Implimented".to_string(), true),
+            (
+                "Offensive Wars",
+                pnw::link(
+                    "Not Implimented".to_string(),
+                    format!(
+                        "https://politicsandwar.com/nation/id={}&display=war",
+                        nation.id
+                    ),
+                ),
+                true,
+            ),
+            (
+                "Defensive Wars",
+                pnw::link(
+                    "Not Implimented".to_string(),
+                    format!(
+                        "https://politicsandwar.com/nation/id={}&display=war",
+                        nation.id
+                    ),
+                ),
+                true,
+            ),
+            (
+                "Actions",
+                format!(
+                    "{}{}{}{}{}",
+                    pnw::link(
+                        ":e_mail:".to_string(),
+                        format!(
+                            "{}{}",
+                            "https://politicsandwar.com/inbox/message/receiver=", nation.leader
+                        ),
+                    ),
+                    pnw::link(
+                        ":outbox_tray:".to_string(),
+                        format!(
+                            "{}{}",
+                            "https://politicsandwar.com/nation/trade/create/nation=", nation.name
+                        ),
+                    ),
+                    pnw::link(
+                        ":no_entry:".to_string(),
+                        format!(
+                            "{}{}",
+                            "https://politicsandwar.com/index.php?id=68&name=", nation.name
+                        ),
+                    ),
+                    pnw::link(
+                        ":crossed_swords:".to_string(),
+                        format!(
+                            "{}{}",
+                            "https://politicsandwar.com/nation/war/declare/id=", nation.id
+                        ),
+                    ),
+                    pnw::link(
+                        ":detective:
+                        "
+                        .to_string(),
+                        format!(
+                            "{}{}",
+                            "https://politicsandwar.com/nation/espionage/eid=", nation.id
+                        ),
+                    )
+                ),
+                true,
+            ),
         ])
     })
 }
